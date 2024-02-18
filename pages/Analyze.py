b5ba6b06715c3681276f54fba5d9439fb2f439bc
pspiagicw <pspiagicw@gmail.com>
2024-02-18 12:15:35 +0530

    feat: updated requirements.txt

--- pages/Analyze.py
+++ pages/Analyze.py
@@ -2,15 +2,18 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('database.csv')
+st.title("Analysis")
+st.header("Here's a analysis of your daily spending")
+st.divider()
 
+df = pd.read_csv('database.csv')
 
 df1 = df.groupby("Date").sum()
 
 df1 = df1.reset_index()
-print(df1)
 st.line_chart(data = df1,  x = "Date",y = "Amount")
 
+st.divider()
 
 item_prices = df.groupby('Tags')['Amount'].sum()
 
@@ -18,10 +21,8 @@ total_price = item_prices.sum()
 
 item_percentages = (item_prices / total_price) * 100
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(5, 5))
 ax.pie(item_percentages, labels=item_percentages.index, autopct='%1.1f%%', startangle=90)
-fig.set_figwidth(20)
-fig.set_figheight(20)
 ax.axis('equal')
 
 
