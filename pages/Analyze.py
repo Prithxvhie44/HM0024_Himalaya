421aeda68bcf60b9812d71aba5eb1452f56996d3
pspiagicw <pspiagicw@gmail.com>
2024-02-18 11:49:26 +0530

    feat: analyze.py

--- pages/Analyze.py
+++ pages/Analyze.py
@@ -2,38 +2,27 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r"C:\Users\Vedant Hirekar\Downloads\expenserecords.csv")
+df = pd.read_csv('database.csv')
 
 
-df1 = df.groupby("date").sum()
+df1 = df.groupby("Date").sum()
 
-col1, col2 = st.columns(2, gap = "large")
-
-with col1:
-    df1 = df1.reset_index()
-    print(df1)
-    st.line_chart(data = df1,  x = "date",y = "price")
+df1 = df1.reset_index()
+print(df1)
+st.line_chart(data = df1,  x = "Date",y = "Amount")
 
 
-with col2:
-    item_prices = df.groupby('item')['price'].sum()
+item_prices = df.groupby('Tags')['Amount'].sum()
 
-    total_price = item_prices.sum()
+total_price = item_prices.sum()
 
-    item_percentages = (item_prices / total_price) * 100
+item_percentages = (item_prices / total_price) * 100
 
-
-    fig, ax = plt.subplots()
-    ax.pie(item_percentages, labels=item_percentages.index, autopct='%1.1f%%', startangle=90)
-    ax.axis('equal')
-
-
-    st.pyplot(fig)
+fig, ax = plt.subplots()
+ax.pie(item_percentages, labels=item_percentages.index, autopct='%1.1f%%', startangle=90)
+fig.set_figwidth(20)
+fig.set_figheight(20)
+ax.axis('equal')
 
 
-
-
-
-
-
-
+st.pyplot(fig)
