63e364f59d1000297923ae4724e536c1753aeb3f
Prithxvhie44 <143012269+Prithxvhie44@users.noreply.github.com>
2024-02-18 10:06:45 +0530

    Update speechlib.py

--- speechlib.py
+++ speechlib.py
@@ -3,21 +3,20 @@ import speech_recognition as sr
 # Initialize the recognizer 
 r = sr.Recognizer() 
  
-# Exception handling to handle
-# exceptions at the runtime
+# Defined function for speech recognition
 def getPhrase():
     try:
+       #Load the audio file
         audiofile = sr.AudioFile("audio.wav")
 
         with audiofile as source:
-        # use the microphone as source for input.
              
             # wait for a second to let the recognizer
             # adjust the energy threshold based on
             # the surrounding noise level 
             r.adjust_for_ambient_noise(source, duration=0.2)
              
-            #listens for the user's input 
+            # Creates a audio source out of the wav file
             audio2 = r.listen(source, phrase_time_limit=20)
              
             # Using google to recognize audio
