cbd0134acf6500791d9f7ba6f5b5fbdbbb8c1ee4
Prithxvhie44 <143012269+Prithxvhie44@users.noreply.github.com>
2024-02-18 10:10:31 +0530

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import nltk
 
+# Downloaind models
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
+# Parse the sentence using NLP to get keywords and amount
 def getEntry(text):
-    tokenized = nltk.word_tokenize(text)
+    # Tokenize using nltk
+    tokenized = nltk.word_tokenize(text
+    # Perform POS Tagging using Penn Treebank codeset
     pos = nltk.pos_tag(tokenized)
     entry = identifyEntry(pos)
     return entry
@@ -13,6 +17,9 @@ def getEntry(text):
 def identifyEntry(tags):
     nouns = list()
     amount = None
+    # Only keep nouns and cardinal number.
+    # Noun is recognized by NNS, NN, NP, NPS
+    # So we see if first letter is N, if yes, we add it to the tags.
     for item in tags:
         value, tag = item
         value = str(value)
