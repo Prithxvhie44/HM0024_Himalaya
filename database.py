14173028ec8651ca7a1eaaa8ad87b41df969e5a0
pspiagicw <pspiagicw@gmail.com>
2024-02-22 12:19:45 +0530

    feat: database.py also can add row

--- database.py
+++ database.py
@@ -6,5 +6,11 @@ def getData():
     df = pd.read_sql_query("select * from users;", conn)
     return df
 
+def addRow(date, time, username, description, category, amount):
+    df = getData()
+    df.loc[len(df.index)] = [date, time, username, description, category, amount]
+    conn = sqlite3.connect("database.db")
+    df.to_sql('users', conn, if_exists='replace')
+
 
 
